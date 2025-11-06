f5939106e27c8d741afb13d62b2d168c82116f67
Mr. Z. <zazaalaza@users.noreply.github.com>
2025-11-06 20:32:50 +0100

    Proper Module System and Headers

--- iosMath/IosMath.h
+++ iosMath/IosMath.h
@@ -7,8 +7,26 @@
 //   
 //  This software may be modified and distributed under the terms of the
 //  MIT license. See the LICENSE file for details.
+//
 
+//! Project version number for iosMath.
+FOUNDATION_EXPORT double iosMathVersionNumber;
+
+//! Project version string for iosMath.
+FOUNDATION_EXPORT const unsigned char iosMathVersionString[];
+
+// Main public API - Math rendering view
 #import <IosMath/MTMathUILabel.h>
-#import <IosMath/MTMathListDisplay.h>
+
+// Core math data structures
 #import <IosMath/MTMathList.h>
 #import <IosMath/MTMathListBuilder.h>
+#import <IosMath/MTMathListDisplay.h>
+
+// Font management
+#import <IosMath/MTFont.h>
+#import <IosMath/MTFontManager.h>
+
+// Advanced APIs for programmatic math list creation
+#import <IosMath/MTMathAtomFactory.h>
+#import <IosMath/MTMathListIndex.h>
